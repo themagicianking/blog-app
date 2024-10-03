8072579b70e819b1b66f29625032d986cae50b26
Thane Chaves <tmoonder@gmail.com>
2024-10-03 18:24:06 -0400

    Page now refreshes with new post after submitting.

--- client/src/components/CreatePost.jsx
+++ client/src/components/CreatePost.jsx
@@ -75,7 +75,11 @@ export default function CreatePost() {
                   <button
                     className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                     type="button"
-                    onClick={() => {setShowModal(false), setSubmitted(false)}}
+                    onClick={() => {
+                      setShowModal(false),
+                        setSubmitted(false),
+                        location.reload();
+                    }}
                   >
                     Close
                   </button>
