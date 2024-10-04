1e9b6ab28ab522bc5e24f2bab0c8c4a9e807977c
Thane Chaves <tmoonder@gmail.com>
2024-10-03 20:20:53 -0400

    Got tailwind working correctly. (Broken commit; layout is not correct and needs to be rearranged.)

--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -1,10 +1,42 @@
-export default function Post({ id, body, author, createdat }) {
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  Typography,
+} from "@material-tailwind/react";
+
+export default function Post({ src, alt, author, title, body }) {
   return (
-    <li key={id}>
-      <p>{body}</p>
-      <p>
-        Posted by {author} at {createdat}
-      </p>
-    </li>
+    <Card className="w-full max-w-[48rem] flex-row">
+      <CardHeader
+        shadow={false}
+        floated={false}
+        className="m-0 w-2/5 shrink-0 rounded-r-none"
+      >
+        <img src={src} alt={alt} className="h-full w-full object-cover" />
+      </CardHeader>
+      <CardBody>
+        <Typography variant="h6" color="gray" className="mb-4 uppercase">
+          {author}
+        </Typography>
+        <Typography variant="h4" color="blue-gray" className="mb-2">
+          {title}
+        </Typography>
+        <Typography color="gray" className="mb-8 font-normal">
+          {body}
+        </Typography>
+      </CardBody>
+    </Card>
   );
 }
+
+// export default function Post({ id, body, author, createdat }) {
+//   return (
+//     <li key={id}>
+//       <p>{body}</p>
+//       <p>
+//         Posted by {author} at {createdat}
+//       </p>
+//     </li>
+//   );
+// }
